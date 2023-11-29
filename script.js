cc37c1516b274364667cae2694f4dce404c0371f
k1llgray <hermesjgcode@gmail.com>
2023-11-29 20:26:13 -0300

    add game()

--- script.js
+++ script.js
@@ -28,3 +28,28 @@ function playRound(playerSelection, computerSelection) {
 function updateScore() {
     document.getElementById("score").innerText = `Score: Player ${playerScore} - Computer ${computerScore}`;
 }
+
+function game() {
+    for (let i = 0; i < 5; i++) {
+        let playerSelection;
+        do {
+            playerSelection = prompt("Choose: Rock, Paper, or Scissors?").toLowerCase();
+        } while (!["rock", "paper", "scissors"].includes(playerSelection));
+
+        const computerSelection = getComputerChoice();
+        const roundResult = playRound(playerSelection, computerSelection);
+        alert(roundResult);
+    }
+
+    if (playerScore > computerScore) {
+        alert("You win the game!");
+    } else if (playerScore < computerScore) {
+        alert("You lose the game!");
+    } else {
+        alert("The game ends in a tie!");
+    }
+    
+    playerScore = 0;
+    computerScore = 0;
+    updateScore();
+}
