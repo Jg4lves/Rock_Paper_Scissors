07891c0c5a5cac1669edfe7dda83bf18501c7310
k1llgray <hermesjgcode@gmail.com>
2023-11-29 19:36:56 -0300

    add playRound()

--- script.js
+++ script.js
@@ -1,4 +1,25 @@
+let playerScore = 0;
+let computerScore = 0;
+
 function getComputerChoice() {
     const choices = ["rock", "paper", "scissors"];
     return choices[Math.floor(Math.random() * 3)];
+}
+
+function playRound(playerSelection, computerSelection) {
+    playerSelection = playerSelection.toLowerCase();
+
+    if (
+        (playerSelection === "rock" && computerSelection === "scissors") ||
+        (playerSelection === "paper" && computerSelection === "rock") ||
+        (playerSelection === "scissors" && computerSelection === "paper")
+    ) {
+        playerScore++;
+        return `You Win! ${playerSelection} beats ${computerSelection}.`;
+    } else if (playerSelection === computerSelection) {
+        return `Tie! Play again."`
+    } else {
+        computerScore++;
+        return `You lost! ${computerSelection} beats ${playerSelection}.`;
+    }
 }
